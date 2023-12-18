1a2e5dab3cca80c7fb74ff0570cf74fa733337a4
Mehmet Demir <mehmet.demir@student.manchester.ac.uk>
2023-12-18 00:07:25 +0000

    Fix spacing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,24 @@
 import React from "react";
-import NavBar from './components/NavBar'
-import AllEntries from './routes/AllEntries'
-import NewEntry from './routes/NewEntry'
-import EditEntry from './routes/EditEntry'
-import { EntryProvider } from './utilities/globalContext'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import AllEntries from "./routes/AllEntries";
+import EditEntry from "./routes/EditEntry";
+import NewEntry from "./routes/NewEntry";
+import { EntryProvider } from "./utilities/globalContext";
 
 export default function App() {
   return (
     <section className="bg-slate-50 dark:bg-slate-600 min-h-screen">
-  <Router>
-    <EntryProvider>
-    <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<AllEntries/>}>
-        </Route>
-        <Route path="create" element={<NewEntry/>}>
-        </Route>
-        <Route path="edit/:id" element={<EditEntry/>}>
-        </Route>
-      </Routes>
-    </EntryProvider>
-    </Router>
+      <Router>
+        <EntryProvider>
+          <NavBar></NavBar>
+          <Routes>
+            <Route path="/" element={<AllEntries />}></Route>
+            <Route path="create" element={<NewEntry />}></Route>
+            <Route path="edit/:id" element={<EditEntry />}></Route>
+          </Routes>
+        </EntryProvider>
+      </Router>
     </section>
-    
   );
 }
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,22 @@
-import {NavLink} from 'react-router-dom'
-import DarkToggle from './DarkToggle'
+import { NavLink } from "react-router-dom";
+import DarkToggle from "./DarkToggle";
 
-export default function NavBar(){
-    return(
-      <nav className="flex justify-center gap-5">
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 dark:bg-slate-700 dark:hover:bg-slate-800 hover:bg-blue-500 rounded-md font-medium text-white dark:text-gray-100" to={'/'}>All Entries</NavLink>
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 dark:bg-slate-700 dark:hover:bg-slate-800 hover:bg-blue-500 rounded-md font-medium text-white dark:text-gray-100" to={'/create'}>New Entry</NavLink>
-        <DarkToggle></DarkToggle>
-      </nav>
-    )
+export default function NavBar() {
+  return (
+    <nav className="flex justify-center gap-5">
+      <NavLink
+        className="m-3 p-4 text-xl bg-blue-400 dark:bg-slate-700 dark:hover:bg-slate-800 hover:bg-blue-500 rounded-md font-medium text-white dark:text-gray-100"
+        to={"/"}
+      >
+        All Entries
+      </NavLink>
+      <NavLink
+        className="m-3 p-4 text-xl bg-blue-400 dark:bg-slate-700 dark:hover:bg-slate-800 hover:bg-blue-500 rounded-md font-medium text-white dark:text-gray-100"
+        to={"/create"}
+      >
+        New Entry
+      </NavLink>
+      <DarkToggle></DarkToggle>
+    </nav>
+  );
 }
