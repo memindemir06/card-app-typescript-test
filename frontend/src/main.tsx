e232e1a82a8ab5799a01cbdc07cd5e734ee58feb
Mehmet Demir <mehmet.demir@student.manchester.ac.uk>
2023-12-18 00:04:11 +0000

    Add DarkToggle component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './App.css'
-import { DarkModeProvider } from './utilities/globalContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./App.css";
+import { DarkModeProvider } from "./utilities/globalContext";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <DarkModeProvider>
       <App />
     </DarkModeProvider>
   </React.StrictMode>
-)
+);
